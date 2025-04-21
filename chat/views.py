e095777926259eb01b8beb9ef3b3871ba497987c
Anibal Sanchez <anibal.sanchez@doceleguas.com>
2025-04-20 23:07:30 -0400

    Adjustment in settings. Removed unused code.

--- chat/views.py
+++ chat/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from .models import ModelType, Thread, Prompt, Model
+from .models import Thread, Prompt, Model
 from .serializers import CustomTokenObtainPairSerializer, ModelSerializer, SignupSerializer, ThreadSerializer, PromptSerializer
 from .aichat_factory import LangChainModel
 from collections import defaultdict
